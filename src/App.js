4b15a0f101b8e8d26ac609584939f2b24dfd5a1e
NotArtwork <AntArce3@gmail.com>
2022-07-20 14:04:52 -0400

    added Dynamic backgrounds

--- src/App.js
+++ src/App.js
@@ -4,9 +4,19 @@ import Fighter from './components/Fighter';
 
 function App() {
   const fighters = [
-  'Inkling','Lucina', 'Mario','Yoshi',
-  'Luigi', 'Snake', 'Fox', 'Marth',
-  'Sora', 'Ike','Cloud', 'Zelda' ]
+    {name: 'Inkling',color:'orange'},
+    {name: 'Lucina',color:'blue'},
+    {name: 'Mario',color:'darkred'},
+    {name: 'Yoshi',color:'lightgreen'},
+    {name: 'Luigi',color:'green'},
+    {name: 'Snake',color:'gray'},
+    {name: 'Fox',color:'purple'},
+    {name: 'Marth',color:'lightskyblue'},
+    {name: 'Sora',color:'red'},
+    {name: 'Ike',color:'darkblue'},
+    {name: 'Cloud',color:'white'},
+    {name: 'Peach',color:'pink'},
+]
   return (
     <div className='App'>
       <h1>Fighters</h1>
