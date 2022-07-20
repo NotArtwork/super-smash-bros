635015663757a9af1adbcf1979277d072cfa6741
NotArtwork <AntArce3@gmail.com>
2022-07-20 13:47:31 -0400

    Added Fighter component

---
+++ src/components/Fighter.js
@@ -0,0 +1,12 @@
+
+
+const Fighter = ({fighter}) => {
+    return (
+    <div>
+        <img src={`https://www.smashbros.com/assets_v2/img/fighter/thumb_a/${fighter.toLowerCase()}.png`} className='fighter-image' />
+        <h5 className='fighter-name'>{fighter}</h5>
+    </div>
+    )
+}
+
+export default Fighter
