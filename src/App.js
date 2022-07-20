c79fd8d357040e4112eb644f889367927f81236b
NotArtwork <AntArce3@gmail.com>
2022-07-20 11:24:51 -0400

    Setup fighters array

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const fighters = [
+  'Inkling','Lucina', 'Mario','Yoshi',
+  'Luigi', 'Snake', 'Fox', 'Marth',
+  'Sora', 'Ike','Cloud', 'Zelda' ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Fighters</h1>
+      <div className='Fighters-grid'>
+
+      </div>
+
     </div>
   );
 }
