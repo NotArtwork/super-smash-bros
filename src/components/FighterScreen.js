7bcd86f5c6dfadf2f55a3ab95625790364413234
NotArtwork <AntArce3@gmail.com>
2022-07-21 16:05:41 -0400

    Setup css

--- src/components/FighterScreen.js
+++ src/components/FighterScreen.js
@@ -2,8 +2,10 @@
 
 const FighterScreen = () => {
     return (
-        <div>
-            <h1>Fighter Screen</h1>
+        <div id='fighter-screen'>
+            <img src='' className='char-model' />
+            <h3 className="char-name"></h3>
+
         </div>
     )
 
